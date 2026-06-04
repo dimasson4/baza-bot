d0adff1e524157904080be54fdcfd463f404fcbc
dimasson4 <dimasson44@gmail.com>
2026-06-04 21:05:11 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ def handle_market_log(message):
             return
 
         # 3. АПТЕЧНЫЙ РАСЧЕТ МАТРИЦЫ ОРДЕРОВ ПО ФОРМУЛАМ
-        balance = 65.54  # Базовый баланс fallback
+        balance = 10.00  # Базовый баланс fallback
         lot = round((balance * 0.02) / (18.50 * 1.02), 3)
         if lot < 0.001:
             lot = 0.001
